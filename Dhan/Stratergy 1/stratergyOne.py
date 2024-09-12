98d7275148c9df69eb9455bc6f62a6d02de4e05b
Pavan Raj K G <nesarastudio0492@gmail.com>
2024-09-12 13:19:36 +0530

    Update stratergyOne.py

--- Dhan/Stratergy 1/stratergyOne.py
+++ Dhan/Stratergy 1/stratergyOne.py
@@ -4,4 +4,14 @@ Created on Thu Sep 12 11:28:09 2024
 
 @author: Administrator
 """
+from dhanhq import dhanhq
+from dhanhq import marketfeed 
 
+access_token = ""
+client_ID = ""
+
+dhan = dhanhq(client_ID,access_token)
+
+Fund_details = dhan.get_fund_limits()
+
+print(Fund_details['data']['availabelBalance'])
